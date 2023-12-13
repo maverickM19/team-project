ae8f1bd4f56abfe7187e4d9ab7ea7bfce63545d8
Nazarii Prokopchuk <nazarii.prokopchuk.pz.2021@lpnu.ua>
2023-12-13 22:46:03 +0200

    Nazarii first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import NazariiImg from "./img/nazarii-img.webp";
+
 function App() {
   return (
     <div className="App">
       <h3>Team Project</h3>
       <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
-        <div></div>
-        <div></div>
+        <div>
+          <h3>Nazarii Prokopchuk</h3>
+          <p>Role: QA</p>
+          <img
+            style={{ width: "100px", height: "100px" }}
+            src={NazariiImg}
+          ></img>
+        </div>
+        <div></div>пше
         <div></div>
         <div></div>
       </div>
