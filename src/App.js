2155c3e6d1f9a4055692c25f06e65e93946ada3f
Nazarii Prokopchuk <nazarii.prokopchuk.pz.2021@lpnu.ua>
2023-12-13 22:21:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Team Project</h3>
+      <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
